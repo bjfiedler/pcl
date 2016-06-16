9fa331a3d7e58d592cf837cf8fcdbd123e9d4aa5
Christian K <chriskoetsier@t-online.de>
2016-06-16 16:39:38 +0200

    Distanzbestimmung, Bessere Objekterkennung

--- PCL - PlayerDetection/src/de/pcl/smartshirt/FindPlayer.java
+++ PCL - PlayerDetection/src/de/pcl/smartshirt/FindPlayer.java
@@ -3,9 +3,14 @@ package de.pcl.smartshirt;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.image.BufferedImage;
+import java.awt.image.DataBuffer;
+import java.awt.image.DataBufferByte;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
@@ -13,8 +18,15 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JSlider;
 
-import org.opencv.core.*;
-import org.opencv.imgproc.*;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.Point;
+import org.opencv.core.RotatedRect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
 import org.opencv.videoio.VideoCapture;
  
 public class FindPlayer {
@@ -95,7 +107,21 @@ public class FindPlayer {
         	boolean firstRun = true;
         	
             while (true) {        
-                if (camera.read(frame)) {
+               //if (camera.read(frame)) {
+            		
+            		Thread.sleep(100);
+            	
+            		BufferedImage image = null;
+					try {
+						image = ImageIO.read(new File("pics/blau.jpg"));
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					
+            		byte[] pixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
+            		frame.put(0, 0, pixels);
+                	
                 	Mat hsvFrame = new Mat();
                 	Imgproc.cvtColor(frame, hsvFrame, Imgproc.COLOR_BGR2HSV);             	
                 	
@@ -121,11 +147,34 @@ public class FindPlayer {
                 	bwFrame = imErode(bwFrame, 0, 5);
                 	bwFrame = imDialte(bwFrame, 0, 5);                	
                 	
-//                	List<Rect> rects = findRectContour(bwFrame);
-//                	for(Rect r:rects) {
-//                    	//Imgproc.rectangle(bwFrame, new Point(r.x,r.y), new Point(r.x+r.width,r.y+r.height), new Scalar(255, 0, 0), 5);
-//                    	Imgproc.rectangle(bwFrame, r.tl(), r.br(), new Scalar(255, 0, 0), 3, 8, 0);
-//                	}
+                	List<RotatedRect> rects = findRectContour(bwFrame);
+                	
+                	//Convert to color pic
+                	Imgproc.cvtColor(bwFrame, bwFrame, Imgproc.COLOR_GRAY2BGR);
+                	
+                	for(RotatedRect r:rects) {
+                		Point[] pt = new Point[4];
+                		r.points(pt);
+             
+                		Scalar color = new Scalar(0, 0, 255);                		
+                		for(int i=0; i<4; i++) {
+                			Imgproc.line(bwFrame, pt[i], pt[(i+1)%4], color, 3, 8, 0);
+                		}           
+                		
+                		Imgproc.circle(bwFrame, r.center, 2, color, 3);
+                		               		
+                		//Rect RotatedRect = r.boundingRect();
+                    	//Imgproc.rectangle(bwFrame, new Point(r.x,r.y), new Point(r.x+r.width,r.y+r.height), new Scalar(255, 0, 0), 5);
+                    	//Imgproc.rectangle(bwFrame, RotatedRect.tl(), RotatedRect.br(), new Scalar(255, 0, 0), 3, 8, 0);
+                	}
+                	
+                	if(rects.size() == 1) {
+                		//System.out.println("Distance: " + getDistance(rects.get(0).center, rects.get(1).center));
+                		
+                		getDirection(rects.get(0));
+                		
+                		
+                	}
                 	
 //                	//Team 2 - Farbbereich grün
 //                	Mat destFrame = new Mat();
@@ -150,8 +199,8 @@ public class FindPlayer {
                     }
                 }
             }   
-        }
-        camera.release();	
+        //}
+        //camera.release();	
 	}
 	
 	public static Mat imErode(Mat srcImg, int erosionElem, int erosionSize) {
@@ -189,15 +238,17 @@ public class FindPlayer {
 		return destImg;
 	}
 	
-	public static List<Rect> findRectContour(Mat srcImg) {
+	public static List<RotatedRect> findRectContour(Mat srcImg) {		
 		List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
 		Mat hierarchy = new Mat();
-		Imgproc.findContours(srcImg, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+		Imgproc.findContours(srcImg.clone(), contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
 		
 		//TODO: NOCH FALSCHE CONTOUREN DENKE ICH!!! SONST SOLLTE ES KLAPPEN! 
 		//SIEHE: http://answers.opencv.org/question/25755/drawing-bounding-box-in-java/
 		
-		List<Rect> rectList = new ArrayList<Rect>();		
+		//List<Rect> rectList = new ArrayList<Rect>();
+		List<RotatedRect> rectList = new ArrayList<RotatedRect>();
+		
 		MatOfPoint2f approxCurve = new MatOfPoint2f();
 		for(int i = 0; i< contours.size(); i++) {			
 			//Convert contours(i) from MatOfPoint to MatOfPoint2f
@@ -210,9 +261,34 @@ public class FindPlayer {
 	        MatOfPoint points = new MatOfPoint(approxCurve.toArray());
 
 	        //Get bounding rect of contour
-	        Rect rect = Imgproc.boundingRect(points);
-			rectList.add(rect);
+	        //Rect rect = Imgproc.boundingRect(points);
+			RotatedRect rect = Imgproc.minAreaRect(contour2f);
+	        rectList.add(rect);
 		}
 		return rectList;		
 	}
+	
+	
+	
+	public static double getDistance(Point p1, Point p2) {
+		return Math.sqrt(Math.pow(p1.x-p2.x, 2) + Math.pow(p1.y-p2.y, 2));
+	}
+	
+	public static double getDirection(RotatedRect rect1) {
+		double dir = 0;
+		
+		System.out.println(rect1.angle);
+		
+		return dir;
+	}
+	
+	public static double getAngle(RotatedRect rect) {
+		return 0;		
+	}
+	
+	
+	public static void generateAlert() {
+		
+	}
+	
 }
