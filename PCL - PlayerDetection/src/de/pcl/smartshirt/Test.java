b7e906b73c1129d359f043596365041f7cd147a5
Christian K <chriskoetsier@t-online.de>
2016-06-16 14:45:34 +0200

    initial commit

---
+++ PCL - PlayerDetection/src/de/pcl/smartshirt/Test.java
@@ -0,0 +1,81 @@
+package de.pcl.smartshirt;
+
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.awt.image.WritableRaster;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+
+import org.opencv.core.Mat;
+       
+public class Test extends JPanel {
+    //Load an image
+    public BufferedImage loadImage(String file) {
+        BufferedImage img;
+
+        try {
+            File input = new File(file);
+            img = ImageIO.read(input);
+
+            return img;
+        } catch (Exception e) {
+            System.out.println("erro");
+        }
+
+        return null;
+    }
+
+    //Save an image
+    public void saveImage(BufferedImage img) {        
+        try {
+            File outputfile = new File("Images/new.png");
+            ImageIO.write(img, "png", outputfile);
+        } catch (Exception e) {
+            System.out.println("error");
+        }
+    }
+
+    //Grayscale filter
+    public BufferedImage grayscale(BufferedImage img) {
+        for (int i = 0; i < img.getHeight(); i++) {
+            for (int j = 0; j < img.getWidth(); j++) {
+                Color c = new Color(img.getRGB(j, i));
+
+                int red = (int) (c.getRed() * 0.299);
+                int green = (int) (c.getGreen() * 0.587);
+                int blue = (int) (c.getBlue() * 0.114);
+
+                Color newColor =
+                        new Color(
+                        red + green + blue,
+                        red + green + blue,
+                        red + green + blue);
+
+                img.setRGB(j, i, newColor.getRGB());
+            }
+        }
+
+        return img;
+    }
+
+    public BufferedImage MatToBufferedImage(Mat frame) {
+        //Mat() to BufferedImage
+        int type = 0;
+        if (frame.channels() == 1) {
+            type = BufferedImage.TYPE_BYTE_GRAY;
+        } else if (frame.channels() == 3) {
+            type = BufferedImage.TYPE_3BYTE_BGR;
+        }
+        BufferedImage image = new BufferedImage(frame.width(), frame.height(), type);
+        WritableRaster raster = image.getRaster();
+        DataBufferByte dataBuffer = (DataBufferByte) raster.getDataBuffer();
+        byte[] data = dataBuffer.getData();
+        frame.get(0, 0, data);
+
+        return image;
+    }
+
+}
